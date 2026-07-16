8ffb0d94e5c7717177999467e4af76a9a06265a1
naveenxd <naveenxd@devh.in>
2026-07-16 17:42:53 +0530

    feat: implement AFK module with MongoDB persistence and in-memory caching

--- models/afk.go
+++ models/afk.go
@@ -1,5 +1,10 @@
 package models
 
+import "time"
+
 type AFKState struct {
-	// todo
+	UserID   int64     `bson:"_id"`
+	Username string    `bson:"username,omitempty"`
+	AFKTime  time.Time `bson:"afk_time"`
+	Reason   string    `bson:"reason,omitempty"`
 }
